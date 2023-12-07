b92761cb6fbde9c29514b0ceaaffcb266943bc20
3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P <57987480+3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P@users.noreply.github.com>
2023-12-07 12:59:04 +0900

    Add

---
+++ get.sh
@@ -0,0 +1,8 @@
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/delete_these_files.tar.gz
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/delete_this_01.txt
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/delete_this_02.txt
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/delete_this_03.txt
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/keep_us.tar.gz
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/keep_me_01.txt
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/keep_me_02.txt
+wget https://github.com/3Vay5fX6kc78R8NZMT5r8SXTdPxbdJ7P/recovery/raw/main/keep_me_03.txt
